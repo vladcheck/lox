d1e85cbd2c672c29a5c649f114bf4727a5aa4881
vladcheck <valdemarcheck@yandex.ru>
2026-01-19 00:13:50 +0300

    feat: add OP_CONSTANT

--- clox/chunk.c
+++ clox/chunk.c
@@ -1,17 +1,19 @@
 #include <stdlib.h>
+
 #include "chunk.h"
-#include "memory.h"
 
 void initChunk(Chunk *chunk)
 {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+    initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk *chunk)
 {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    freeValueArray(&chunk->constants);
     initChunk(chunk); // reset the fields
 }
 
@@ -26,4 +28,11 @@ void writeChunk(Chunk *chunk, uint8_t byte)
 
     chunk->code[chunk->count] = byte;
     chunk->count++;
+}
+
+int addConstant(Chunk *chunk, Value value)
+{
+    writeValueArray(&chunk->constants, value);
+    int appendIndex = chunk->constants.count - 1;
+    return appendIndex;
 }
--- clox/chunk.h
+++ clox/chunk.h
@@ -1,11 +1,13 @@
 #ifndef clox_chunk_h
 #define clox_chunk_h
 
-#include "common.h"
+#include "memory.h"
+#include "value.h"
 
 typedef enum
 {
-    OP_RETURN
+    OP_RETURN,
+    OP_CONSTANT,
 } OpCode;
 
 // Chunk acts like a dynamic array
@@ -14,10 +16,12 @@ typedef struct
     int count; // bytes in use
     int capacity;
     uint8_t *code;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk *chunk);
 void freeChunk(Chunk *chunk);
 void writeChunk(Chunk *chunk, uint8_t byte);
+int addConstant(Chunk *chunk, Value value);
 
 #endif
