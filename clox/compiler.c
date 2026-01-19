7be0900745c216564b3ea78d1dc9c506f60b1f36
vladcheck <valdemarcheck@yandex.ru>
2026-01-19 15:55:55 +0300

    feat: define different data types

--- clox/compiler.c
+++ clox/compiler.c
@@ -194,6 +194,24 @@ static void binary()
     }
 }
 
+static void literal()
+{
+    switch (parser.previous.type)
+    {
+    case TOKEN_FALSE:
+        emitByte(OP_FALSE);
+        break;
+    case TOKEN_NIL:
+        emitByte(OP_NIL);
+        break;
+    case TOKEN_TRUE:
+        emitByte(OP_TRUE);
+        break;
+    default:
+        return; // Unreachable.
+    }
+}
+
 static void expression()
 {
     parsePrecedence(PREC_ASSIGNMENT);
@@ -208,7 +226,7 @@ static void grouping()
 static void number()
 {
     double value = strtod(parser.previous.start, NULL);
-    emitConstant(value);
+    emitConstant(NUMBER_VAL(value));
 }
 
 static void unary()
@@ -255,17 +273,17 @@ ParseRule rules[] = {
     [TOKEN_AND] = {NULL, NULL, PREC_NONE},
     [TOKEN_CLASS] = {NULL, NULL, PREC_NONE},
     [TOKEN_ELSE] = {NULL, NULL, PREC_NONE},
-    [TOKEN_FALSE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_FALSE] = {literal, NULL, PREC_NONE},
     [TOKEN_FOR] = {NULL, NULL, PREC_NONE},
     [TOKEN_FUN] = {NULL, NULL, PREC_NONE},
     [TOKEN_IF] = {NULL, NULL, PREC_NONE},
-    [TOKEN_NIL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_NIL] = {literal, NULL, PREC_NONE},
     [TOKEN_OR] = {NULL, NULL, PREC_NONE},
     [TOKEN_PRINT] = {NULL, NULL, PREC_NONE},
     [TOKEN_RETURN] = {NULL, NULL, PREC_NONE},
     [TOKEN_SUPER] = {NULL, NULL, PREC_NONE},
     [TOKEN_THIS] = {NULL, NULL, PREC_NONE},
-    [TOKEN_TRUE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_TRUE] = {literal, NULL, PREC_NONE},
     [TOKEN_VAR] = {NULL, NULL, PREC_NONE},
     [TOKEN_WHILE] = {NULL, NULL, PREC_NONE},
     [TOKEN_XOR] = {NULL, NULL, PREC_NONE},
--- clox/vm.c
+++ clox/vm.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,6 +12,20 @@ static void resetStack(VM *vm)
     vm->stackTop = vm->stack;
 }
 
+static void runtimeError(VM *vm, const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+    fputs("\n", stderr);
+
+    size_t instruction = vm->ip - vm->chunk->code - 1;
+    int line = vm->chunk->lines[instruction];
+    fprintf(stderr, "[line %d] in script\n", line);
+    resetStack(vm);
+}
+
 void initVM(VM *vm)
 {
     vm->chunk = NULL;
@@ -22,68 +37,100 @@ void freeVM(VM *vm)
 {
 }
 
+static Value peek(VM *vm, int distance)
+{
+    return vm->stackTop[-1 - distance];
+}
+
 InterpretResult run(VM *vm)
 {
 #define READ_BYTE() (*vm->ip++)
 #define READ_CONSTANT() (vm->chunk->constants.values[READ_BYTE()])
-#define BINARY_OP(op)       \
-    do                      \
-    {                       \
-        double b = pop(vm); \
-        double a = pop(vm); \
-        push(vm, a op b);   \
+#define BINARY_OP(vm, valueType, op)                            \
+    do                                                          \
+    {                                                           \
+        if (!IS_NUMBER(peek(vm, 0)) || !IS_NUMBER(peek(vm, 1))) \
+        {                                                       \
+            runtimeError(vm, "Operands must be numbers.");      \
+            return INTERPRET_RUNTIME_ERROR;                     \
+        }                                                       \
+        double b = AS_NUMBER(pop(vm));                          \
+        double a = AS_NUMBER(pop(vm));                          \
+        push(vm, valueType(a op b));                            \
     } while (false)
-
-    for (;;)
-    {
 #ifdef DEBUG_TRACE_EXECUTION
-        printf("\t");
-        for (Value *slot = vm->stack; slot < vm->stackTop; slot++)
-        {
-            printf("[ ");
-            printValue(*slot);
-            printf(" ]");
-        }
-        printf(" ");
-        disassembleInstruction(vm->chunk, (int)(vm->ip - vm->chunk->code));
+    printf("\t");
+    for (Value *slot = vm->stack; slot < vm->stackTop; slot++)
+    {
+        printf("[ ");
+        printValue(*slot);
+        printf(" ]");
+    }
+    printf(" ");
+    disassembleInstruction(vm->chunk, (int)(vm->ip - vm->chunk->code));
 #endif
 
-        uint8_t instruction;
-        switch (instruction = READ_BYTE())
-        {
-        case OP_CONSTANT:
-        {
-            Value constant = READ_CONSTANT();
-            push(vm, constant);
-            break;
-        }
-        case OP_CONSTANT_LONG:
-        {
-            Value constant = READ_CONSTANT();
-            push(vm, constant);
-            break;
-        }
-        case OP_NEGATE:
-            *vm->stackTop = -*vm->stackTop;
-            break;
-        case OP_ADD:
-            BINARY_OP(+);
-            break;
-        case OP_SUBTRACT:
-            BINARY_OP(-);
-            break;
-        case OP_MULTIPLY:
-            BINARY_OP(*);
-            break;
-        case OP_DIVIDE:
-            BINARY_OP(/);
-            break;
-        case OP_RETURN:
-            printValue(pop(vm));
-            printf("\n");
-            return INTERPRET_OK;
-        }
+    uint8_t instruction;
+    switch (instruction = READ_BYTE())
+    {
+    case OP_CONSTANT:
+    {
+        Value constant = READ_CONSTANT();
+        push(vm, constant);
+        break;
     }
+    case OP_CONSTANT_LONG:
+    {
+        Value constant = READ_CONSTANT();
+        push(vm, constant);
+        break;
+    }
+    case OP_NIL:
+    {
+        push(vm, NIL_VAL);
+        break;
+    }
+    case OP_TRUE:
+    {
+        push(vm, BOOL_VAL(true));
+        break;
+    }
+    case OP_FALSE:
+    {
+        push(vm, BOOL_VAL(false));
+        break;
+    }
+    case OP_NEGATE:
+    {
+        if (!IS_NUMBER(peek(vm, 0)))
+        {
+            runtimeError(vm, "Operand must be a number.");
+            return INTERPRET_RUNTIME_ERROR;
+        }
+        push(vm, NUMBER_VAL(-AS_NUMBER(pop(vm))));
+        break;
+    }
+    case OP_ADD:
+        BINARY_OP(vm, NUMBER_VAL, +);
+        break;
+    case OP_SUBTRACT:
+        BINARY_OP(vm, NUMBER_VAL, -);
+        break;
+    case OP_MULTIPLY:
+        BINARY_OP(vm, NUMBER_VAL, *);
+        break;
+    case OP_DIVIDE:
+        BINARY_OP(vm, NUMBER_VAL, /);
+        break;
+    case OP_RETURN:
+    {
+        printValue(pop(vm));
+        printf("\n");
+        return INTERPRET_OK;
+    }
+    }
+
+    return INTERPRET_OK; // unreachable
 
 #undef READ_BYTE
 #undef READ_CONSTANT
